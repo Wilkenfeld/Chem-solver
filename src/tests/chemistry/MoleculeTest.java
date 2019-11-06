7061b666d360e73a862cceab04c57d3bb722d967
JackAlone04 <giacalone293@gmail.com>
2019-11-06 20:22:40 +0100

    Updated tests.

--- src/tests/chemistry/MoleculeTest.java
+++ src/tests/chemistry/MoleculeTest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.Test;
 public class MoleculeTest {
     @Test
     public void testMoleculeShape() {
+
+        // Carbon monoxide.
         OxygenAtom ox1 = new OxygenAtom();
         OxygenAtom ox2 = new OxygenAtom();
         CarbonAtom car = new CarbonAtom();
@@ -30,5 +32,24 @@ public class MoleculeTest {
         }
 
         assertEquals(Molecule.ShapeEnum.LineShape, molecule.getMoleculeShape());
+
+        // Methane.
+        Atom[] list2 = new Atom[5];
+        list2[0] = new CarbonAtom();
+        list2[1] = new HydrogenAtom();
+        list2[2] = new HydrogenAtom();
+        list2[3] = new HydrogenAtom();
+        list2[4] = new HydrogenAtom();
+
+        try {
+            molecule = new Molecule(list2);
+            molecule.calculateShape();
+        } catch (IllegalMoleculeException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
+        assertEquals(Molecule.ShapeEnum.SquareShape, molecule.getMoleculeShape());
+
     }
 }
