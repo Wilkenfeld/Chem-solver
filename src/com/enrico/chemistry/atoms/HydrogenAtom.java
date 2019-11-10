2f6a0763e850eeca282e94ba81d41a5fd285f1c7
JackAlone04 <giacalone293@gmail.com>
2019-11-10 09:46:49 +0100

    Chenged Hydrogen class and added Sulfur.

--- src/com/enrico/chemistry/atoms/HydrogenAtom.java
+++ src/com/enrico/chemistry/atoms/HydrogenAtom.java
@@ -9,9 +9,8 @@ public class HydrogenAtom extends Atom {
     public static final double ATOMIC_MASS = 1.008;
     public static final double ELECTRONEGATIVITY = 2.2;
 
-    // NB: even if Hydrogen is not a metal, since it's located in that column it will be classified as one here.
     public HydrogenAtom() {
         super(ATOM_SYMBOL, ATOM_NAME, ATOMIC_NUMBER, ATOMIC_MASS, ELECTRONEGATIVITY,
-              BINDING_ELECTRONS, 0, AtomClassType.AlkalineMetals);
+              BINDING_ELECTRONS, 0, AtomClassType.NotMetals);
     }
 }
---
+++ src/com/enrico/chemistry/atoms/SulfurAtom.java
@@ -0,0 +1,15 @@
+package com.enrico.chemistry.atoms;
+
+public class SulfurAtom extends Atom {
+    public static final String ATOM_SYMBOL = "S";
+    public static final String ATOM_NAME = "Sulfur";
+    public static final int ATOMIC_NUMBER = 16;
+    public static final int BINDING_ELECTRONS = 6;
+    public static final double ATOMIC_MASS = 32.07;
+    public static final double ELECTRONEGATIVITY = 2.58;
+
+    public SulfurAtom() {
+        super(ATOM_SYMBOL, ATOM_NAME, ATOMIC_NUMBER, ATOMIC_MASS, ELECTRONEGATIVITY,
+                BINDING_ELECTRONS, 2, AtomClassType.NotMetals);
+    }
+}
