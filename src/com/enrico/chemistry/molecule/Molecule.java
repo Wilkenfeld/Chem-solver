b4412e35a3001f093d19807ffaecd911b6d2caec
JackAlone04 <giacalone293@gmail.com>
2019-11-14 18:02:25 +0100

    Added new case for line molecule.

--- src/com/enrico/chemistry/molecule/Molecule.java
+++ src/com/enrico/chemistry/molecule/Molecule.java
@@ -138,7 +138,8 @@ public class Molecule {
                  (bindedAtoms.size() == 3 && doubletsNumber == 1))
             moleculeShape = ShapeEnum.PyramidShape;
         else if ((bindedAtoms.size() == 2 && doubletsNumber == 0) ||
-                 (bindedAtoms.size() == 1 && doubletsNumber == 0))
+                 (bindedAtoms.size() == 1 && doubletsNumber == 0) ||
+                 (bindedAtoms.size() == 2 && doubletsNumber == 1))
             moleculeShape = ShapeEnum.LineShape;
         else if ((bindedAtoms.size() == 3 && doubletsNumber == 2) ||
                  (bindedAtoms.size() == 3 && doubletsNumber == 0))
