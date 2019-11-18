ffc7193e25cbebb5f1e6a648cc77a17137979b49
JackAlone04 <giacalone293@gmail.com>
2019-11-18 15:50:57 +0100

    Added custom combobox to window.

--- src/com/enrico/windows/main/problems/biology/monosaccharidestypes/BiologyMonosaccharidesTypesProblemWindow.java
+++ src/com/enrico/windows/main/problems/biology/monosaccharidestypes/BiologyMonosaccharidesTypesProblemWindow.java
@@ -1,6 +1,8 @@
 package com.enrico.windows.main.problems.biology.monosaccharidestypes;
 
 import com.enrico.widgets.buttons.DefaultButton;
+import com.enrico.widgets.combobox.DefaultComboBox;
+import com.enrico.widgets.combobox.DefaultComboBoxItem;
 import com.enrico.widgets.menu.ProblemWindowMenuBar;
 import com.enrico.windows.BasicWindow;
 
@@ -13,14 +15,23 @@ public class BiologyMonosaccharidesTypesProblemWindow extends BasicWindow {
     private JTextField nVariableTxtField;
     private JLabel monosaccharideTypeLbl;
     private DefaultButton moreInfoButton;
-    private JComboBox molecularTypeComboBox;
+    private DefaultComboBox moleculeTypeComboBox;
 
     public static final String TITLE = "Monosaccharides types";
     public static final String BIOLOGY_MONOSACCHARIDES_PROBLEM_IDENTIFIER =
             "BIOLOGY_MONOSACCHARIDES_PROBLEM_IDENTIFIER";
 
+    private static final String ALDOSE_TXT = "Aldose";
+    private static final String ALDOSE_VAL = "ALDOSE_VAL";
+
+    private static final String KETONE_TXT = "Ketone";
+    private static final String KETONE_VAL = "KETONE_VAL";
+
+    private static final DefaultComboBoxItem[] moleculeTypes = new DefaultComboBoxItem[2];
+
     public BiologyMonosaccharidesTypesProblemWindow() {
         super(TITLE);
+        setResizable(false);
 
         ProblemWindowMenuBar problemWindowMenuBar = new ProblemWindowMenuBar(this);
         setJMenuBar(problemWindowMenuBar);
@@ -36,7 +47,11 @@ public class BiologyMonosaccharidesTypesProblemWindow extends BasicWindow {
     }
 
     private void createUIComponents() {
+        moleculeTypes[0] = new DefaultComboBoxItem(ALDOSE_TXT, ALDOSE_VAL);
+        moleculeTypes[1] = new DefaultComboBoxItem(KETONE_TXT, KETONE_VAL);
+
         moreInfoButton = new DefaultButton("More info");
+        moleculeTypeComboBox = new DefaultComboBox(moleculeTypes);
     }
 
     private void calculateMonosaccharideType(String variableN) {
@@ -45,7 +60,5 @@ public class BiologyMonosaccharidesTypesProblemWindow extends BasicWindow {
         int carbonNumber, hydrogenNumber, oxygenNumber;
         carbonNumber = oxygenNumber = intN;
         hydrogenNumber = intN * 2;
-
-
     }
 }
