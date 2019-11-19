4ffc09ad5f1ddbf9b8f00c65df71c3bc90672f21
JackAlone04 <giacalone293@gmail.com>
2019-11-19 08:23:09 +0100

    Changed combobox font.

--- src/com/enrico/widgets/combobox/DefaultComboBox.java
+++ src/com/enrico/widgets/combobox/DefaultComboBox.java
@@ -1,18 +1,24 @@
 package com.enrico.widgets.combobox;
 
+import com.enrico.interfaces.FontInterface;
+
 import javax.swing.*;
 import java.util.ArrayList;
 
 @SuppressWarnings("unchecked")
-public class DefaultComboBox extends JComboBox {
+public class DefaultComboBox extends JComboBox implements FontInterface {
     public DefaultComboBox(DefaultComboBoxItem[] items) {
         for (DefaultComboBoxItem item : items)
             addItem(item);
+
+        setFont(normalTextFont);
     }
 
     public DefaultComboBox(ArrayList<DefaultComboBoxItem> items) {
         for (DefaultComboBoxItem item : items)
             addItem(item);
+
+        setFont(normalTextFont);
     }
 
     private void createUIComponents() {
