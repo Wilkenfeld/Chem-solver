4b7de38d50fdaad3eb87f188c17e80c5bfba7e21
JackAlone04 <giacalone293@gmail.com>
2019-12-08 09:58:51 +0100

    Added license.

--- src/com/enrico/chemistry/molecule/atomgroup/AtomGroup.java
+++ src/com/enrico/chemistry/molecule/atomgroup/AtomGroup.java
@@ -1,26 +1,28 @@
+/*
+ * Chem solver. A multi-platform chemistry and physics problem solver.
+ *  Copyright (C) 2019  Giacalone Enrico
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.enrico.chemistry.molecule.atomgroup;
 
 import com.enrico.chemistry.molecule.shapedmolecule.ShapedMolecule;
 
 import java.util.ArrayList;
 
-/**
- * This class represents a group of AtomPlaceCards. This makes the algorithm more flexible,
- * because every "group" of atom swill be stored in here and will be stored here.
- *
- * What I mean by "group" is something as suck molecule:
- *      H   H
- *      |   |
- * H----C---C----H
- *      |   |
- *      H   H
- *
- * To represent this we will make two groups: the both with 3 Hydrogen atoms and 1 carbon, eah with its
- * own coordinates.
- *
- * Then we will iterate inside an ArrayList of groups, and we will iterate
- * inside the AtomPlaceCard ArrayList to get the atoms properties to display and the position.
- */
 public class AtomGroup {
     private ArrayList<ShapedMolecule.AtomPlaceCard> atoms;
 
