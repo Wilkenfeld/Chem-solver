70cb6b16093df8231c449158376d7b11f04cadc0
JackAlone04 <giacalone293@gmail.com>
2020-01-21 18:25:42 +0100

    Added custom cursor when inserting atoms.

--- src/com/enrico/widgets/canvas/moleculedrawingcanvas/MoleculeDrawingCanvas.java
+++ src/com/enrico/widgets/canvas/moleculedrawingcanvas/MoleculeDrawingCanvas.java
@@ -3,10 +3,21 @@ package com.enrico.widgets.canvas.moleculedrawingcanvas;
 import com.enrico.widgets.canvas.GenericCanvas;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public final class MoleculeDrawingCanvas extends GenericCanvas {
+public final class MoleculeDrawingCanvas extends GenericCanvas implements MouseListener {
+    private final Cursor drawingCursor;
+
     public MoleculeDrawingCanvas() {
         super();
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image cursorImageRaw = toolkit.getImage(getClass().getClassLoader().getResource("cursor_assets/molecule_builder_insert_cursor.png"));
+        Image cursorImage = cursorImageRaw.getScaledInstance(45, 45, 0);
+
+        drawingCursor = toolkit.createCustomCursor(cursorImage, new Point(1, 1), "cursor_image");
+        setCursor(drawingCursor);
     }
 
     @Override
@@ -15,5 +26,31 @@ public final class MoleculeDrawingCanvas extends GenericCanvas {
     }
 
     public void createUIComponents() {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+        setCursor(drawingCursor);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+        // Set the default cursor.
+        setCursor(Cursor.getDefaultCursor());
     }
 }
