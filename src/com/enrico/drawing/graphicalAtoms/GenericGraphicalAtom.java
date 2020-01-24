15676e0825dc4724071e3d80421fd60b44ea8d24
JackAlone04 <giacalone293@gmail.com>
2020-01-24 14:29:59 +0100

    Added basic properties for graphical atom.

--- src/com/enrico/drawing/graphicalAtoms/GenericGraphicalAtom.java
+++ src/com/enrico/drawing/graphicalAtoms/GenericGraphicalAtom.java
@@ -3,8 +3,38 @@ package com.enrico.drawing.graphicalAtoms;
 import com.enrico.chemistry.atoms.GenericAtom;
 
 public abstract class GenericGraphicalAtom extends GenericAtom {
+    protected int x;
+    protected int y;
+
+    protected final String imagePath;
+
     public GenericGraphicalAtom(String symbol, String completeName, int atomicNumber, double atomicMass, double electronegativity,
-                                int bindingElectronsNumber, int doublets, int ionizationEnergy, AtomClassType classType) { // PLACEHOLDER.
+                                int bindingElectronsNumber, int doublets, int ionizationEnergy, AtomClassType classType,
+                                int xPos, int yPos, String imagePath) {
         super(symbol, completeName, atomicNumber, atomicMass, electronegativity, bindingElectronsNumber, doublets, ionizationEnergy, classType);
+
+        x = xPos;
+        y = yPos;
+        this.imagePath = imagePath;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
--- src/com/enrico/drawing/graphicalAtoms/GraphicalCarbonAtom.java
+++ src/com/enrico/drawing/graphicalAtoms/GraphicalCarbonAtom.java
@@ -3,7 +3,11 @@ package com.enrico.drawing.graphicalAtoms;
 import com.enrico.interfaces.atoms.CarbonAtomInterface;
 
 public final class GraphicalCarbonAtom extends GenericGraphicalAtom implements CarbonAtomInterface {
-    public GraphicalCarbonAtom() {
-        super (ATOM_SYMBOL, ATOM_NAME, ATOMIC_NUMBER, ATOMIC_MASS, ELECTRONEGATIVITY, BINDING_ELECTRONS, DOUBLETS, IONIZATION_NUMBER, CLASS_TYPE);
+    public static final String IMAGE_PATH_STRING =
+            "atom_icons/nonmetals/atom_icon_carbon.png";
+
+    public GraphicalCarbonAtom(int x, int y) {
+        super (ATOM_SYMBOL, ATOM_NAME, ATOMIC_NUMBER, ATOMIC_MASS, ELECTRONEGATIVITY, BINDING_ELECTRONS, DOUBLETS,
+               IONIZATION_NUMBER, CLASS_TYPE, x, y, IMAGE_PATH_STRING);
     }
 }
