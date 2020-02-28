06336472818f1cbe8f8c786886fbff935ad041d5
JackAlone04 <giacalone293@gmail.com>
2020-02-28 09:55:36 +0100

    Added license and fixed small error in atom binding electrons.

--- src/com/enrico/drawing/graphicalAtoms/semimetals/GraphicalArsenicAtom.java
+++ src/com/enrico/drawing/graphicalAtoms/semimetals/GraphicalArsenicAtom.java
@@ -1,3 +1,22 @@
+/*
+ * Chem solver. A multi-platform chemistry and physics problem solver.
+ *  Copyright (C) 2019 - 2020  Giacalone Enrico
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.enrico.drawing.graphicalAtoms.semimetals;
 
 import com.enrico.drawing.graphicalAtoms.GenericGraphicalAtom;
@@ -7,7 +26,7 @@ public final class GraphicalArsenicAtom extends GenericGraphicalAtom implements 
     public static final String IMAGE_PATH_STRING =
             "atom_icons/semimetals/atom_icon_arsenic.png";
 
-    public static final int STD_BINDINGS = 5;
+    public static final int STD_BINDINGS = 3;
 
     public GraphicalArsenicAtom(int startX, int startY, int endX, int endY, String atomId) {
         super (ATOM_SYMBOL, ATOM_NAME, ATOMIC_NUMBER, ATOMIC_MASS, ELECTRONEGATIVITY, BINDING_ELECTRONS, DOUBLETS,
